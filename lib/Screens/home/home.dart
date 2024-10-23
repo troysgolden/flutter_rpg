e491d5683eaa2300f9d7de81f47aebdde1d3e897
Troy S. Golden <ts8769@gmail.com>
2024-10-23 15:44:33 -0400

    Back To Your Characters Again

--- lib/Screens/home/home.dart
+++ lib/Screens/home/home.dart
@@ -12,7 +12,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Characters Test'),
+        title: const Text('Your Characters'),
         centerTitle: true,
       ),
       body: Container(
