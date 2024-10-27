81e7e773cd8431245a2babe3c6daa451aeb8b0cd
troysgolden <ts8769@gmail.com>
2024-10-27 19:29:54 -0400

    Finished 3`

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_rpg/models/stats.dart';
+import 'package:flutter_rpg/models/vocation.dart';
 
 class Character with Stats {
   // constructor
   Character({
     required this.name,
     required this.slogan,
+    required this.vocation,
     required this.id,
   });
 
   // fields
+  final Vocation vocation;
   final String name;
   final String slogan;
   final String id;
