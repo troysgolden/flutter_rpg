2cb559032c8a5e92de061d37ed767df669d0f849
Troy S. Golden <ts8769@gmail.com>
2024-11-05 17:06:37 -0500

    40 Done

--- lib/Screens/create/create.dart
+++ lib/Screens/create/create.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/vocation.dart';
+import 'package:flutter_rpg/screens/create/vocation_card.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
@@ -35,10 +37,10 @@ class _CreateState extends State<Create> {
     print('Name: ${_nameController.text}');
     print('Slogan: ${_sloganController.text}');
   }
-@override
-  Widget build(BuildContext context) {
 
-return Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const StyledTitle('Character Creation'),
         centerTitle: true,
@@ -85,10 +87,34 @@ return Scaffold(
             ),
           ),
           const SizedBox(height: 30),
+          //select vocation title
+
+          Center(
+            child: Icon(Icons.code, color: AppColors.primaryColor),
+          ),
+          const Center(child: StyledHeading('Choose your vocation')),
+          const Center(
+              child: StyledText('This determines your stats and skills.')),
+          const SizedBox(height: 30),
+          //input for name and slogan,
+          const SizedBox(height: 30),
+          //vocation cards
+          const VocationCard(
+            vocation: Vocation.junkie,
+          ),
+           const VocationCard(
+            vocation: Vocation.ninja,
+          ),
+           const VocationCard(
+            vocation: Vocation.raider,
+          ),
+           const VocationCard(
+            vocation: Vocation.wizard,
+          ),
           //create button
           Center(
             child: StyledButton(
-              onPressed:  handleSubmit,
+              onPressed: handleSubmit,
               child: const StyledHeading('Create Character'),
             ),
           ),
---
+++ lib/Screens/create/vocation_card.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/vocation.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+
+class VocationCard extends StatelessWidget {
+  const VocationCard({
+    super.key,
+    required this.vocation,
+  });
+
+  final Vocation vocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          children: [
+            Image.asset(
+              'assets/img/vocations/${vocation.image}',
+              width: 80,
+              colorBlendMode: BlendMode.color,
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  StyledHeading(vocation.title),
+                  StyledText(vocation.description),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
