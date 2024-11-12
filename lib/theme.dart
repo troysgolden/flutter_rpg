c235e93f4f9b8b7f50c6f7c8e9b7a426ec7f4667
troysgolden <ts8769@gmail.com>
2024-11-12 16:18:40 -0500

    Done 49

--- lib/theme.dart
+++ lib/theme.dart
@@ -63,4 +63,9 @@ ThemeData primaryTheme = ThemeData(
     labelStyle: TextStyle(color: AppColors.textColor),
     prefixIconColor: AppColors.textColor,
   ),
+  //dialog theme,
+  dialogTheme: DialogTheme(
+    backgroundColor: AppColors.secondaryAccent,
+    surfaceTintColor: Colors.transparent,
+  ),
 );
