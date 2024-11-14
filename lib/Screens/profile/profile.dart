c0a56a9a036353b5c6e12811c01cbe3481dd0fa3
Troy S. Golden <ts8769@gmail.com>
2024-11-14 17:13:41 -0500

    done 52

--- lib/Screens/profile/profile.dart
+++ lib/Screens/profile/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/screens/profile/stats_table.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 
@@ -70,6 +71,13 @@ class Profile extends StatelessWidget {
             ),
 
             //stats and skills,
+            Container(
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    StatsTable(character),
+                  ],
+                ))
 
             //save button
           ],
---
+++ lib/Screens/profile/stats_table.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
+
+class StatsTable extends StatefulWidget {
+  const StatsTable(this.character, {super.key});
+
+  final Character character;
+
+  @override
+  State<StatsTable> createState() => _StatsTableState();
+}
+
+class _StatsTableState extends State<StatsTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            //available points
+            Container(
+              color: AppColors.secondaryColor,
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Icon(Icons.star,
+                      color: widget.character.points > 0
+                          ? Colors.yellow
+                          : Colors.grey),
+                  const SizedBox(width: 20),
+                  const StyledText('Stats points available: '),
+                  const Expanded(child: SizedBox(width: 20)),
+                  StyledHeading(widget.character.points.toString()),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
