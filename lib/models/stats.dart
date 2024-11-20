b787ed5caf614f7500a1c2c4fa0269eb271435a5
Troy S. Golden <ts8769@gmail.com>
2024-11-20 16:15:39 -0500

    54 Done

--- lib/models/stats.dart
+++ lib/models/stats.dart
@@ -1,54 +1,62 @@
 mixin Stats {
+  int _points = 10;
   int _health = 10;
   int _attack = 10;
   int _defense = 10;
   int _skill = 10;
-  int _points = 10;
 
-  // Getters
+  // getters
   int get points => _points;
-  Map<String, int> get statsAsMap => {
-        "health": _health,
-        "attack": _attack,
-        "defense": _defense,
-        "skill": _skill
-      };
-  List<Map<String, String>> get statsAsFormattedList => [
-        {"title": "Health", "value": _health.toString()},
-        {"title": "Attack", "value": _attack.toString()},
-        {"title": "Defense", "value": _defense.toString()},
-        {"title": "Skill", "value": _skill.toString()}
-      ];
 
+  List<Map<String, String>> get statsAsFormattedList => [
+    {'title': 'health', 'value': _health.toString()},
+    {'title': 'attack', 'value': _attack.toString()},
+    {'title': 'defense', 'value': _defense.toString()},
+    {'title': 'skill', 'value': _skill.toString()}
+  ];
+
+  Map<String, int> get statsAsMap => {
+    "health": _health,
+    "attack": _attack,
+    "defense": _defense,
+    "skill": _skill,
+  };
+
+  // methods
   void increaseStat(String stat) {
     if (_points > 0) {
-      if (stat == "health") {
+      if (stat == 'health') {
         _health++;
-      } else if (stat == "attack") {
+      }
+      if (stat == 'attack') {
         _attack++;
-      } else if (stat == "defense") {
+      }
+      if (stat == 'defense') {
         _defense++;
-      } else if (stat == "skill") {
+      }
+      if (stat =='skill') {
         _skill++;
       }
+      _points--;
     }
   }
 
   void decreaseStat(String stat) {
-    if (_points > 0) {
-      if (stat == "health" && _health > 5) {
-        _health--;
-        _points++;
-      } else if (stat == "attack" && _attack > 5) {
-        _attack--;
-        _points++;
-      } else if (stat == "defense" && _defense > 5) {
-        _defense--;
-        _points++;
-      } else if (stat == "skill" && _skill > 5) {
-        _skill--;
-        _points++;
-      }
+    if (stat == 'health' && _health > 5) {
+      _health--;
+      _points++;
+    }
+    if (stat == 'attack' && _attack > 5) {
+      _attack--;
+      _points++;
+    }
+    if (stat == 'defense' && _defense > 5) {
+      _defense--;
+      _points++;
+    }
+    if (stat == 'skill' && _skill > 5) {
+      _skill--;
+      _points++;
     }
   }
 }
