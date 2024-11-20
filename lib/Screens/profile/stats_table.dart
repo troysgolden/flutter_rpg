5f5ea7fa32e359b73babe025ad22dd1fa201a0be
Troy S. Golden <ts8769@gmail.com>
2024-11-20 16:16:58 -0500

    54 Done

--- lib/Screens/profile/stats_table.dart
+++ lib/Screens/profile/stats_table.dart
@@ -16,27 +16,86 @@ class _StatsTableState extends State<StatsTable> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            //available points
-            Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          // available points
+          Container(
               color: AppColors.secondaryColor,
               padding: const EdgeInsets.all(8),
               child: Row(
                 children: [
-                  Icon(Icons.star,
-                      color: widget.character.points > 0
-                          ? Colors.yellow
-                          : Colors.grey),
+                  Icon(
+                    Icons.star,
+                    color: widget.character.points > 0
+                        ? Colors.yellow
+                        : Colors.grey,
+                  ),
                   const SizedBox(width: 20),
-                  const StyledText('Stats points available: '),
-                  const Expanded(child: SizedBox(width: 20)),
-                  StyledHeading(widget.character.points.toString()),
+                  const StyledText('Stat points available:'),
+                  const Expanded(child: SizedBox()),
+                  StyledHeading(widget.character.points.toString())
                 ],
-              ),
-            ),
-          ],
-        ));
+              )),
+
+          // stats table
+          Table(
+              children: widget.character.statsAsFormattedList.map((stat) {
+            return TableRow(
+                decoration: BoxDecoration(
+                    color: AppColors.secondaryColor.withOpacity(0.5)),
+                children: [
+                  // stat title (e.g. health)
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: StyledHeading(stat['title']!),
+                    ),
+                  ),
+
+                  // stat value
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: StyledHeading(stat['value']!),
+                    ),
+                  ),
+
+                  // icon to increase stat
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: IconButton(
+                        icon: Icon(Icons.arrow_upward,
+                            color: AppColors.textColor),
+                        onPressed: () {
+                          setState(() {
+                            widget.character.increaseStat(stat['title']!);
+                          });
+                        },
+                        ),
+                  ),
+
+                  // icon to decrease stat
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_downward,
+                          color: AppColors.textColor),
+                      onPressed: () {
+                        setState(() {
+                          widget.character.decreaseStat(stat['title']!);
+                        });
+                      },
+                    ),
+                  ),
+                ]
+                );
+          }).toList()
+          ),
+        ],
+      ),
+    );
   }
 }
