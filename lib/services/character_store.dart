e3ce501bec58bd1a16fc2f2298bc49ae0c2513bf
Troy S. Golden <ts8769@gmail.com>
2024-12-04 16:30:15 -0500

    done 60

---
+++ lib/services/character_store.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/models/vocation.dart';
+
+class CharacterStore extends ChangeNotifier {
+  final List<Character> _characters = [
+    Character(
+      id: "1",
+      name: "Jezic",
+      slogan: "Fire It Up",
+      vocation: Vocation.junkie,
+    ),
+    Character(
+      id: "2",
+      name: "Nexus",
+      slogan: "Fast and stealthy",
+      vocation: Vocation.ninja,
+    ),
+    Character(
+      id: "3",
+      name: "Wylan",
+      slogan: "Abracadabra",
+      vocation: Vocation.wizard,
+    ),
+    Character(
+      id: "4",
+      name: "Raynal",
+      slogan: "Lets Get Hyped",
+      vocation: Vocation.raider,
+    ),
+  ];
+  get characters => _characters;
+}
