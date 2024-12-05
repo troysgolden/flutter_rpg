c20e443d2a77e66cc8a28423617a626cfec9b269
Troy S. Golden <ts8769@gmail.com>
2024-12-05 15:17:00 -0500

    61 Done

--- lib/Screens/home/home.dart
+++ lib/Screens/home/home.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rpg/Screens/create/create.dart';
-import 'package:flutter_rpg/models/character.dart';
+//import 'package:flutter_rpg/Screens/create/create.dart';
+//import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/screens/create/create.dart';
 import 'package:flutter_rpg/screens/home/character_card.dart';
+import 'package:flutter_rpg/services/character_store.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -25,17 +28,22 @@ class _HomeState extends State<Home> {
         child: Column(children: [
           // list of characters
           Expanded(
-            child: ListView.builder(
-                itemCount: characters.length,
-                itemBuilder: (_, index) {
-                  return CharacterCard(characters[index]);
-                }),
+            child: Consumer<CharacterStore>(builder: (context, value, child) {
+              return ListView.builder(
+                  itemCount: value.characters.length,
+                  itemBuilder: (_, index) {
+                    return CharacterCard(value.characters[index]);
+                  });
+            }),
           ),
 
           StyledButton(
             onPressed: () {
               Navigator.push(
-                  context, MaterialPageRoute(builder: (ctx) => const Create()));
+                  context,
+                  MaterialPageRoute(
+                    builder: (ctx) => const CreateScreen(),
+                  ));
             },
             child: const StyledHeading('Create New'),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter_rpg/Screens/create/create.dart';
 import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/services/character_store.dart';
 import 'package:flutter_rpg/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: const Home(),
-    theme: primaryTheme,
+  runApp(ChangeNotifierProvider(
+    create: (context) => CharacterStore(),
+    child: MaterialApp(
+      home: const Home(),
+      theme: primaryTheme,
+    ),
   ));
 }
 
